c47367931303ae2a056c2369a645f9fe584cea13
Inês Justo <inesjusto@ua.pt>
2020-10-21 15:27:26 +0100

    corpus reader

--- Assignment1/CorpusReader.py
+++ Assignment1/CorpusReader.py
@@ -1,3 +1,16 @@
 # 1. Create a corpus reader that iterates over the collection (corpus) of document and returns, in turn, the contents of
 # each document.
 # For this assignment consider only the title and abstract fields and ignore documents with an empty abstract.
+
+import pandas as pd
+import numpy as np
+
+class CorpusReader:
+
+    def __init__(self, csv):
+        self.csv = csv
+
+    def readCorpus(self):
+        data = pd.read_csv(self.csv)
+        corpus = list(zip(data['sha'], data['title'], data['abstract']))
+        clean_collection = [(sha, title, abst) for (sha, title, abst) in corpus if abst is not np.nan]
---
+++ Assignment1/README.md
@@ -0,0 +1,3 @@
+# Assignment 1
+Simple document indexer, consisting of a corpus reader /
+document processor, tokenizer, and indexer.

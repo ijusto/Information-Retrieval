6f2a199b9d441b3f165a3e42e0c358fb76328c6e
Daniel Marques <danielfmarques@ua.pt>
2020-10-26 10:52:00 +0000

    clean up

--- Assignment1/Tokenizer.py
+++ Assignment1/Tokenizer.py
@@ -91,21 +91,3 @@ class BetterTokenizer(Tokenizer):
 
     def getTokens(self):
         return self.terms
-
-
-
-
-
- sentence=str(sentence)
-    sentence = sentence.lower()
-    sentence=sentence.replace('{html}',"") 
-    cleanr = re.compile('<.*?>')
-    cleantext = re.sub(cleanr, '', sentence)
-    rem_url=re.sub(r'http\S+', '',cleantext)
-    rem_num = re.sub('[0-9]+', '', rem_url)
-    tokenizer = RegexpTokenizer(r'\w+')
-    tokens = tokenizer.tokenize(rem_num)  
-    filtered_words = [w for w in tokens if len(w) > 2 if not w in stopwords.words('english')]
-    stem_words=[stemmer.stem(w) for w in filtered_words]
-    lemma_words=[lemmatizer.lemmatize(w) for w in stem_words]
-    return " ".join(filtered_words)

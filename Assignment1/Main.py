111878d51b89c3d50c83a472a7d66318bbb19d48
Inês Justo <inesjusto@ua.pt>
2020-10-29 13:57:20 +0000

    main changes

--- Assignment1/Main.py
+++ Assignment1/Main.py
@@ -2,11 +2,14 @@ from Indexer import Indexer
 
 def main():
     print('before')
-    ind = Indexer("data/all_sources_metadata_2020-03-13.csv", 0).index()
+    simple_ind = Indexer("data/all_sources_metadata_2020-03-13.csv", 0).index()
+    better_ind = Indexer("data/all_sources_metadata_2020-03-13.csv", 1).index()
     print('after')
-    tenOne = Indexer("data/all_sources_metadata_2020-03-13.csv", 0).getTermsInOneDoc()
+    simple_ind.getTermsInOneDoc()
+    better_ind.getTermsInOneDoc()
     print('after')
-    tenHigh = Indexer("data/all_sources_metadata_2020-03-13.csv", 0).getHighestDocFreqTerms()
+    simple_ind.getHighestDocFreqTerms()
+    better_ind.getHighestDocFreqTerms()
     print('after')
 main() 
         

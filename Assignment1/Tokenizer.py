b97c36e3b7a67787f4ba56b9b88218b714ac345a
Inês Justo <inesjusto@ua.pt>
2020-10-29 13:54:12 +0000

    tokenizer changes

--- Assignment1/Tokenizer.py
+++ Assignment1/Tokenizer.py
@@ -26,7 +26,7 @@ class SimpleTokenizer(Tokenizer):
     def getTerms(self):
         # replaces all non-alphabetic characters by a space, lowercases tokens, splits on whitespace
         self.terms = re.split('[\s]', re.sub(r'[^A-Za-z]', ' ', self.title + " " + self.abstract)
-                                                    .lower())
+                              .lower())
         # ignores all tokens with less than 3 characters
         self.terms = list(filter(lambda term: len(term) >= 3, self.terms))
 
@@ -38,46 +38,41 @@ class BetterTokenizer(Tokenizer):
     def __init__(self, title, abstract):
         super().__init__(title, abstract)
 
-    def createTerms(self):
-        for docId in self.doc_map.keys():
-            # split by whitespace
-            self.terms[docId] = re.split('[\s]', self.doc_map[docId][0] + " " + self.doc_map[docId][1])
+    def getTerms(self):
+        # split by whitespace
+        self.terms = re.split('[\s]', self.title + " " + self.abstract)
 
-            # ignore websites
-            if re.match(r'(https?:\/\/)?(www\.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)|(https?:\/\/)?(www\.)?(?!ww)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)', self.doc_map[docId][0]):
-                self.terms[docId] = self.doc_map[docId][0]
+        # ignore websites
+        self.terms = [term for term in self.terms if not re.match(r'(https?:\/\/)?(www\.)[-a-zA-Z0-9@:%._\+~#=]{2,256}'
+                                                                  r'\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)|(https'
+                                                                  r'?:\/\/)?(www\.)?(?!ww)[-a-zA-Z0-9@:%._\+~#=]{2,256}'
+                                                                  r'\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)', term)
+                      # ignore emails
+                      and not re.match(r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$', term)
+                      # ignore words with hyphens and aphostrophes and both
+                      # 's, yea', don't, -yeah, no-, ice-cream, Qu'est-ce, Mary's, High-school, 'tis, Chambers',
+                      # Qu'est-ce, Finland's, isn't, Passengers'
+                      # 2019-2020, COVID-19, receptor-independent
+                      and not re.match(r"'?\w[\w']*(?:-\w+)*'?", term)]
 
-            #ignore emails
-            elif re.match(r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$', self.doc_map[docId][0]):
-                self.terms[docId] = self.doc_map[docId][0]
-
-            #ignore words with hyphens and aphostrophes and both
-            # 's, yea', don't, -yeah, no-, ice-cream, Qu'est-ce, Mary's, High-school, 'tis, Chambers', Qu'est-ce, Finland's, isn't, Passengers'
-            # 2019-2020, COVID-19, receptor-independent
-            elif re.match(r"'?\w[\w']*(?:-\w+)*'?", self.doc_map[docId][0]):
-                self.terms[docId] = self.doc_map[docId][0]
-
-            # dealing with extra pontuation and symbols
-            # ignoring (_) for this type of situation NC_004718.3
-            else:
-                self.terms[docId] =  re.sub(r'[\!\"\#\$\%\&\(\)\*\+\,\:\;\<\>\=\?\[\]\{\}\\\^\`\~\±]+', '',  self.doc_map[docId][0])
-
+        # dealing with extra pontuation and symbols
+        # ignoring (_) for this type of situation NC_004718.3
+        self.terms = [re.sub(r'[\!\"\#\$\%\&\(\)\*\+\,\:\;\<\>\=\?\[\]\{\}\\\^\`\~\±]+', '', term)
+                      for term in self.terms].remove('')
 
         self.stopWordFilter()
         self.stem()
         print(self.terms)
+        return self.terms
 
     def stopWordFilter(self):
         # get the english stopwords
         stopwords = ""
         with open('snowball_stopwords_EN.txt', 'r') as document:
-            stopwords += list(filter(None,re.split("[ \n]", document.read())))
+            stopwords += list(filter(None, re.split("[ \n]", document.read())))
         document.close()
         self.terms = list(filter(lambda term: term not in stopwords, self.terms))
 
     def stem(self):
         stemmer = Stemmer.Stemmer('english')
         self.terms = stemmer.stemWords(self.terms)
-
-    def getTokens(self):
-        return self.terms

22038f4091e321a85e15aa305bfb73333b5341d3
Daniel Marques <danielfmarques@ua.pt>
2020-11-28 14:22:08 +0000

    returns

--- Assignment2/Ranker.py
+++ Assignment2/Ranker.py
@@ -46,10 +46,10 @@ class Ranker:
         # lnc-ltc
         for docId, termsInfo in self.documentsInfo.items():
             documentScore[docId] = sum([lnc_weight*queriesWeights[term] for term, lnc_weight in termsInfo.items()])
-
+        return documentScore
 
     def bm25(self, lenD, avgdl, k1 = 1.2, b = 0.75):
         documentScore = {} # {docId: bm25}
         for docId, termsInfo in self.documentsInfo.items():
             documentScore[docId] = sum([idf*((tf*(k1+1))/(tf + k1 * (1 - b + b * (lenD/avgdl) ))) for _, (idf, tf) in termsInfo.items()])
-
+        return documentScore

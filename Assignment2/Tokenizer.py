ca3469c167f2a45df5cd7aed9992bdad8a50b445
Inês Justo <inesjusto@ua.pt>
2020-11-28 21:40:58 +0000

    .

--- Assignment2/Tokenizer.py
+++ Assignment2/Tokenizer.py
@@ -57,7 +57,7 @@ class BetterTokenizer(Tokenizer):
     def getTerms(self):
         # split by whitespace
         terms = re.split('[\s]', self.text)
-
+        print(self.text, terms)
         for term in terms:
             # maintain websites
             url_match = re.findall(
